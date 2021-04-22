84e9bdaa235407718ea7609233bb197fd08b6305
daudi13 <davouma13@gmail.com>
2021-04-22 04:38:39 +0300

    commit no-scroll on body.active

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,8 @@
 const navIcon = document.querySelector('.hamburger-menu');
 const navBar = document.querySelector('.nav');
+const body = document.querySelector("body")
 
 navIcon.addEventListener('click', () => {
-    navBar.classList.toggle('active')
+    navBar.classList.toggle('active');
+    body.classList.toggle('active');
 })
