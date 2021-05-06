3dce5bb695a14e736e99ac67e3f9e5ff128062ea
daudi13 <davouma13@gmail.com>
2021-05-06 20:49:57 +0300

    commit menu cancel onclick function

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,17 @@
 const navIcon = document.querySelector('.hamburger-menu');
 const navBar = document.querySelector('.nav');
-const body = document.querySelector("body");
+const body = document.querySelector('body');
+const navLinks = document.querySelectorAll('.nav__link');
 
 
 navIcon.addEventListener('click', () => {
-    navBar.classList.toggle('active');
     body.classList.toggle('active');
+    navBar.classList.toggle('active');
 });
 
+navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            body.classList.remove('active');
+            navBar.classList.remove('active');
+        })
+    })
