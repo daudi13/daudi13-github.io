95cf4cb92096de07a8c94e4a42e3b24900fc53c1
daudi13 <davouma13@gmail.com>
2021-05-08 08:06:36 +0300

    commit changes

--- JS/active.js
+++ JS/active.js
@@ -8,7 +8,21 @@ let btns = btnContainer.getElementsByClassName("nav__link");
 
 // loop through the buttons and add the active class to the current/clicked button
 
-for (let i = 0;)
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        let currrent = document.getElementsByClassName("proactive")
+    })
+}
+
+// if there's no active class
+
+if (current.length > 0) {
+    current[0].className = current[0].className.replace("proactive", "");
+}
+
+// add the active class to the current/clicked button
+
+this.className += "proactive";
 
 
 
