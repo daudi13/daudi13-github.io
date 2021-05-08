f70dc83a6595a4c4413d52c48a84942b9a28d360
daudi13 <davouma13@gmail.com>
2021-05-08 06:21:35 +0300

    commit active.js

---
+++ JS/active.js
@@ -0,0 +1,15 @@
+//Get the container element
+
+let btnContainer = document.getElementById("myBtn");
+
+// get all buttons with class = "nav__link" inside the container
+
+let btns = btnContainer.getElementsByClassName("nav__link");
+
+// loop through the buttons and add the active class to the current/clicked button
+
+for (let i = 0;)
+
+
+
+
