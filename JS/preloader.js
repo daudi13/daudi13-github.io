76791824170513f8fb1f914de2a5e74b823ba8e7
daudi13 <davouma13@gmail.com>
2021-05-18 04:47:49 +0300

    commit js

---
+++ JS/preloader.js
@@ -0,0 +1,20 @@
+const preloader = document.querySelector('.preloader');
+const loadingText = document.querySelector('.loading-txt');
+
+let load = 0;
+let int = setInterval(disappear, 30);
+
+function disappear() {
+    load++;
+    if (load > 99) {
+        clearInterval(int);
+    }
+
+    loadingText.innerText = `${load}%`;
+    loadingText.style.opacity = scale(load, 0, 100, 1, 0);
+    preloader.style.opacity = scale(load, 0, 100, 1, 0)
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
